1d8fcec6e8c4f496c1bee843184619e81905c0e7
DentonFrederick <dentontfrederick@gmail.com>
2020-07-29 11:45:13 -0500

    add variable names and stop submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function(){
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(){
+      let pilotName = document.querySelector("input[name=pilot]");
+      let copilot = document.querySelector("input[name=copilot]")
+      let fuelLevel = document.querySelector("input[name=fuelLelvel]")
+      let cargoMass = document.querySelector("input[name=cargoMass]")
+      if(pilotName === "" || copilotName === "" || fuelLevel >= 0 || cargoMass >= 0){
+         alert("ALL FIELDS REQUIRED!!");
+         event.preventDefault();
+      }
 
+   });
+});
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
