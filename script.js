979a0dd0a905e41442878b2ef59039bdecfd54a8
DentonFrederick <dentontfrederick@gmail.com>
2020-08-04 19:52:31 -0500

    added conditionals and the fetch function

--- script.js
+++ script.js
@@ -1,37 +1,66 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function(){
-   let launchForm = document.querySelector("launchForm");
-   form.addEventListener("submit", function(){
-      let pilotName = document.querySelector("input[name=pilot]");
-      let copilotName = document.querySelector("input[name=copilot]")
-      let fuelLevel = document.querySelector("input[name=fuelLelvel]")
-      let cargoMass = document.querySelector("input[name=cargoMass]")
-      if(pilotName === "Chris" && copilotName === "Blake" || isNaN(fuelLevel) || isNaN(cargoMass)){
-         alert("INCORRECT SUBMISSION");
-         event.preventDefault();
-      }
-
+   let pilotName = document.querySelector("input[name=pilotName]");
+   let copilotName = document.querySelector("input[name=copilotName]");
+   let fuelLevel = document.querySelector("input[name=fuelLevel]");
+   let cargoMass = document.querySelector("input[name=cargoMass]");
+   let launchStatus = document.getElementById("launchStatus");
+   let faultyItems = document.getElementById("faultyItems");
+   let pilotStatus = document.getElementById("pilotSatus");
+   let copilotStatus = document.getElementById("copilotStatus");
+   let fuelStatus = document.getElementById("fuelStatus");
+   let cargoStatus = document.getElementById("cargoStatus");
+   let fuelAndCargo = [fuelLevel, cargoMass];
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event){
+      event.preventDefault();
+       if(pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value ===""){
+          window.alert("ALL FIELDS REQUIRED");
+       } else if (!isNaN(pilotName.value) || !isNaN(copilotName.value) || isNaN(fuelLevel.value) || isNaN(cargoMass.value)){
+          window.alert("ENTER VALID INPUTS");
+       } else {
+          if(fuelLevel.value < 10000 || cargoMass > 10000){
+             launchStatus.innerHTML = 'Shuttle not ready for launch';
+             launchStatus.style.color = "red";
+             pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready for launch`;
+             copilotStatus.innerHTML =   `Co-Pilot ${copilotName.value} is ready for launch`;
+             for(let i=0;i<fuelAndCargo.length; i++){
+                if(fuelLevel.value < 10000){
+                   fuelStatus.innerHTML= `NOT ENOUGH FUEL`;
+                  } else {
+                     fuelStatus.innerHTML= `Fuel level high enough for launch`;
+                  }
+                  if(cargoMass.value > 10000){
+                     cargoStatus.innerHTML= `TOO MUCH CARGO`;
+                  } else{;
+                     cargoStatus.innerHTML= `Cargo mass low enough for launch`;
+                  }
+             }
+             faultyItems.style.visibility= "visible";
+          } else {
+             launchStatus.innerHTML= `Shuttle is ready`;
+             launchStatus.style.color= "green";
+             faultyItems.style.visibility= "hidden";
+          }
+       }
    });
-   if(pilotName !== "Chris"){
-      console.log("Not the right captain")
-   }else if(pilotName === "Chris"){
-      console.log("pilot ready")
-   }
-   if(copilotName !== "Blake"){
-      console.log("wrong copilot")
-   } else if(copilotName === "Blake"){
-      console.log("copilot ready")
-   }
-
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then(function(json) {
+         let missionTarget = document.getElementById("missionTarget");
+         let randomSelection = Math.floor(Math.random() * json.length);
+         let mission = json[randomSelection];
+         missionTarget.innerHTML = `
+            <h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${mission.name}</li>
+               <li>Diameter: ${mission.diameter}</li>
+               <li>Star: ${mission.star}</li>
+               <li>Distance from Earth: ${mission.distance}</li>
+               <li>Number of Moons: ${mission.moons}</li>
+            </ol>
+            <img src="${mission.image}">
+         `;
+      });
+   });
 });
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+
